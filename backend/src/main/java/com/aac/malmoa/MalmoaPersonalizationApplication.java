eeee4ce9f330b7b1a9ad1b71b55520641ca45ce7
usernameisuser11 <kds10111@gmail.com>
2026-09-10 13:20:29 +0900

    feat: add backend application entry point

---
+++ backend/src/main/java/com/aac/malmoa/MalmoaPersonalizationApplication.java
@@ -0,0 +1,11 @@
+package com.aac.malmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalmoaPersonalizationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MalmoaPersonalizationApplication.class, args);
+    }
+}
